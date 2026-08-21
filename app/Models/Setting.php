dd8d2bed53991c2016f12b35f01d4d1e7a5a2d5b
AlbertWebs <albertmuhatia@gmail.com>
2026-08-21 13:18:02 +0300

    ..

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -3,25 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Cache;
 
 class Setting extends Model
 {
     protected $fillable = ['key', 'value'];
 
+    /** @var array<string, mixed>|null */
+    protected static ?array $memo = null;
+
     public static function getValue(string $key, mixed $default = null): mixed
     {
-        $all = Cache::remember('app_settings', 60, function () {
-            return static::query()->pluck('value', 'key')->all();
-        });
+        if (static::$memo === null) {
+            static::$memo = static::query()->pluck('value', 'key')->all();
+        }
 
-        return $all[$key] ?? $default;
+        return static::$memo[$key] ?? $default;
     }
 
     public static function setValue(string $key, mixed $value): void
     {
         static::query()->updateOrCreate(['key' => $key], ['value' => is_array($value) ? json_encode($value) : $value]);
-        Cache::forget('app_settings');
+        static::$memo = null;
     }
 
     public static function logoPath(): ?string
